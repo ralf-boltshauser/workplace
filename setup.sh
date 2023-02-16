1a39e6645db2e28b44feed3c6b77ebe00c2db05f
sai04 <sai04@localhost.localdomain>
2023-02-16 15:54:37 +0100

    remove chrome deb

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ sudo apt install code
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
+rm google-chrome-stable_current_amd64.deb
 
 sudo apt update
 sudo apt upgrade
