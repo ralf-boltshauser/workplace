7f05e29001233fcbf92691b0ec911ddd1f9cde72
sai04 <sai04@localhost.localdomain>
2023-02-16 15:47:59 +0100

    install google chrome

--- setup.sh
+++ setup.sh
@@ -9,6 +9,12 @@ sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
+
+## google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+
 sudo apt update
 sudo apt upgrade
 # setting up environment
