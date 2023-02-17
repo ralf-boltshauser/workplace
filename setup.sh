184a79cf67526965a40c888d637006b248221f5e
sai04 <sai04@localhost.localdomain>
2023-02-17 21:51:20 +0100

    Screenshot Functionality

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 # installing tools
 sudo apt update
 sudo apt remove neovim -y
-sudo apt install i3 btop xdotool rofi polybar vim docker.io git  -y
+sudo apt install i3 btop xdotool rofi polybar vim docker.io git  maim xclip -y
 
 ## vs code
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo apt install code -y
 
 ## google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+sudo apt-get install -f -y
 rm google-chrome-stable_current_amd64.deb
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 # setting up environment
 mkdir -p ~/.config
 cp -R i3 ~/.config
